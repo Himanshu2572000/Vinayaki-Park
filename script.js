0aa5516a6f3d965a2e7a1504cf5e199d004c2dfc
Himanshu <halp.raj2572000@gmail.com>
2020-07-21 16:48:37 +0530

    Version - 1.0.0

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sliding Image
+
+// setInterval(() =>{
+//     let imgNum = parseInt(Math.floor(Math.random() * 10));
+//     document.querySelector(`.slideitem${imgNum}`).style.opacity = '1';
+//     document.querySelector(`.slideitem${imgNum}`).style.zindex = '1';
+//     setTimeout(() =>{
+//         document.querySelector(`.slideitem${imgNum}`).style.opacity = '0';
+//         document.querySelector(`.slideitem${imgNum}`).style.zindex = '0';
+//     }, 1999);
+// }, 2000);
